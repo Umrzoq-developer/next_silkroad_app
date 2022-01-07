56136c10fc14436708acaff8c900beef89de6242
Umrzoq Toshkentov <u.toshkentov@SRT.local>
2022-01-07 11:39:57 +0500

    fix: application

--- src/v_components/main_components/Users/index.tsx
+++ src/v_components/main_components/Users/index.tsx
@@ -1,21 +1,17 @@
 // import { useUsersQuery } from "@/graphql/user";
-import React, { lazy, Suspense } from "react";
-// import UserItem from '../UserItem';
+import React from "react";
 import styles from "styles/card.module.scss";
-import UserItem from "../UserItem";
 
 const Users: React.FC = () => {
   // const usersQuery = useUsersQuery();
 
   return (
-    <Suspense fallback={<div>card uploading</div>}>
-      <div className={styles.card__wrapper}>
-        {/* {usersQuery?.data?.users &&
+    <div className={styles.card__wrapper}>
+      {/* {usersQuery?.data?.users &&
           usersQuery?.data?.users?.map((item) => {
             return <UserItem key={item._id} name={item.name} />;
           })} */}
-      </div>
-    </Suspense>
+    </div>
   );
 };
 
